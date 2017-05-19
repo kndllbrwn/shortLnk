d3edb48902d38b7b71b71f0179ab197bd8dcbb8e
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-18 21:09:54 -0400

    tied routes to authentification

--- client/main.js
+++ client/main.js
@@ -2,12 +2,15 @@ import {meteor} from 'meteor/meteor'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
+import { Tracker } from 'meteor/tracker'
 
 import Signup from '../imports/ui/Signup'
 import Links from '../imports/ui/Link'
 import NotFound from '../imports/ui/NotFound'
 import Login from '../imports/ui/Login'
 
+const unauthenticatedPages = ['/', '/signup']
+const authenticatedPages = ['/links']
 const routes = (
   <Router history={browserHistory}>
     <Route path='/' component={Login} />
@@ -16,6 +19,23 @@ const routes = (
     <Route path='*' component={NotFound} />
   </Router>
 )
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId()
+  const pathName = browserHistory.getCurrentLocation().pathname
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathName)
+  const isAuthenticatedPage = authenticatedPages.includes(pathName)
+
+  if (isUnauthenticatedPage && isAuthenticated) {
+    browserHistory.push('/links')
+  } else 
+  if (isAuthenticatedPage && !isAuthenticated) {
+    browserHistory.push('/')
+  }
+
+
+  console.log('isAuthenticated', isAuthenticated)
+})
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'))
