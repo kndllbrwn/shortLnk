5f54052dcac52e332a670b1f75f56fe8c12bc35d
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-19 18:37:05 -0400

    Added simpl-schema package and practiced validation

--- server/main.js
+++ server/main.js
@@ -1,5 +1,52 @@
 import { Meteor } from 'meteor/meteor'
+import SimpleSchema from 'simpl-schema'
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  // const petSchema = new SimpleSchema({
+  //   name: {
+  //     type: String,
+  //     min: 1,
+  //     max: 200
+  //   },
+  //   age: {
+  //     type: Number,
+  //     min: 0
+  //   },
+  //   contactNumber: {
+  //     type: String,
+  //     optional: true,
+  //     regEx: SimpleSchema.RegEx.Phone
+  //   }
+  // })
+
+  // petSchema.validate({
+  //   name: 'Ken',
+  //   age: 3,
+  //   contactNumber: 'ab24#'
+  // })
+
+  // const employeeSchema = new SimpleSchema({
+  //   name: {
+  //     type:String,
+  //     min: 1,
+  //     max: 200
+  //   },
+  //   hourlyWage: {
+  //     type: Number,
+  //     min: 0
+  //   },
+  //   email: {
+  //     type: String,
+  //     regEx: SimpleSchema.RegEx.Email
+  //   }
+  // })
+
+  // employeeSchema.validate({
+  //   name: 'Kb',
+  //   hourlyWage: 20,
+  //   email: 'test@example.com'
+  // })
 })
+
