e2281666e15e05c16582b88430727ccfd9ec635f
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-19 19:03:39 -0400

    Added signup validation

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -14,6 +14,9 @@ export default class Signup extends React.Component {
     let email = this.refs.email.value.trim()
     let password = this.refs.email.value.trim()
 
+    if (password.length < 6) {
+      return this.setState({error: 'Password must be more than 5 characters long.'})
+    }
     Accounts.createUser({email, password}, (err) => {
       if (err) {
         this.setState({error: err.reason})
@@ -29,7 +32,7 @@ export default class Signup extends React.Component {
 
         {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
           <input type='email' ref='email' name='email' placeholder='Email' />
           <input type='password' ref='password' name='password' placeholder='Password' />
           <button>Create Account</button>
--- server/main.js
+++ server/main.js
@@ -4,6 +4,23 @@ import SimpleSchema from 'simpl-schema'
 Meteor.startup(() => {
   // code to run on server at startup
 
+  Accounts.validateNewUser((user) => {
+    const email = user.emails[0].address
+
+    try {
+      new SimpleSchema({
+        email: {
+          type: String,
+          regEx: SimpleSchema.RegEx.Email
+        }
+      }).validate({ email})
+    } catch (e) {
+      throw new Meteor.Error(400, e.message)
+    }
+
+    return true
+  })
+
   // const petSchema = new SimpleSchema({
   //   name: {
   //     type: String,
@@ -49,4 +66,3 @@ Meteor.startup(() => {
   //   email: 'test@example.com'
   // })
 })
-
