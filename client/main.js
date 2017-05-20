438f8dc47dba8edd832c218cc6885acad34be919
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-19 22:12:01 -0400

    set up links collection and inserted into UI in unsecure manner

--- client/main.js
+++ client/main.js
@@ -1,8 +1,9 @@
-import {meteor} from 'meteor/meteor'
+import {Meteor} from 'meteor/meteor'
 import ReactDOM from 'react-dom'
 import { Tracker } from 'meteor/tracker'
 
 import { routes, onAuthChange } from '../imports/routes/routes'
+import { Links } from '../imports/api/links'
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId()
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import { Accounts } from 'meteor/accounts-base'
 
-export default class Links extends React.Component {
+import { Links } from '../api/links'
+import LinksList from './LinksList'
+
+export default class Link extends React.Component {
   logout () {
     Accounts.logout()
+  }
+  onSubmit (e) {
+    const url = this.refs.url.value.trim()
+    e.preventDefault()
+
+    if (url) {
+      Links.insert({ url })
+      this.refs.url.value = ''
+    }
   }
   render () {
     return (
       <div>
         <h1>Lnks</h1>
-
         <button onClick={this.logout.bind(this)}>Logout</button>
+        <LinksList />
+        <p>Add Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type='text' ref='url' placeholder='URL' />
+          <button>Add Link</button>
+        </form>
       </div>
     )
   }
