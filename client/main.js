af55f045c55a764d8e2b7686ed604eac89fb2afd
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-20 23:34:14 -0400

    Added schema validation for links and refactored for users

--- client/main.js
+++ client/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Tracker } from 'meteor/tracker'
 
 import { routes, onAuthChange } from '../imports/routes/routes'
-import { Links } from '../imports/api/links'
+import '../imports/startup/simple-schema-configuration.js'
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId()
