31b0f8473a78e746c034677e8e20a72dac0afd90
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-23 20:30:00 -0400

    Added checkbox to allow users to manipulate filter

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -5,14 +5,17 @@ import { Links } from '../api/links'
 import LinksList from './LinksList'
 import PrivateHeader from './PrivateHeader'
 import AddLink from './AddLink'
+import LinksListFilters from './LinksListFilters'
 
 export default () => {
     return (
       <div>
         <PrivateHeader title="Your Links"/>
+        <LinksListFilters />
         <LinksList />
         <AddLink/>
       </div>
     )
 }
+
 
