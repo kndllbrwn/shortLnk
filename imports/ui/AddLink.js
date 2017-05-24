9db11f6f1fe79e7cfa3f59ae74d7b35894d45733
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-23 23:41:49 -0400

    Updated modal functionality

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -14,34 +14,48 @@ export default class AddLink extends React.Component{
     e.preventDefault()
     console.log(url)
 
-    if (url) {
-      Meteor.call('links.insert', url, (err, res) => {
-        if (!err) {
-          this.setState({ url: '', isOpen: false})
-        }
-      })
-    }
+    
+    Meteor.call('links.insert', url, (err, res) => {
+      if (!err) {
+        this.handleModalClose()
+      } else {
+        this.setState({error: err.reason})
+      }
+    })
+    
   }
   onChange(e){
     this.setState({
       url: e.target.value
     })
   }
+  handleModalClose() {
+    this.setState({
+      url: '',
+      isOpen: false,
+      error: ''})
+  }
   render (){
     return (
       <div>
         <button onClick={() => this.setState({isOpen: true})}>+ Add Link</button>
-        <Modal isOpen={this.state.isOpen} contentLabel="Add Link">
-          <p>Add Link</p>
+        <Modal 
+        isOpen={this.state.isOpen} 
+        contentLabel="Add Link"
+        onAfterOpen={() => this.refs.url.focus()}
+        onRequestClose={this.handleModalClose.bind(this)}>
+          <h1>Add Link</h1>
+          { this.state.error ? <p>{this.state.error}</p> : null }
           <form onSubmit={this.onSubmit.bind(this)}>
             <input 
               type='text'  
               placeholder='URL' 
+              ref="url"
               value={this.state.url}
               onChange={this.onChange.bind(this)}/>
             <button>Add Link</button>
           </form>
-          <button onClick={() => this.setState({url: '', isOpen: false})}>Cancel</button>
+          <button onClick={this.handleModalClose.bind(this)}>Cancel</button>
         </Modal>
       </div>
     )
