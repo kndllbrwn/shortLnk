16601d9fc87f94e16dd7bf7759b50e1e258bc777
kndllbrwn <kndllbrwn1914@gmail.com>
2017-05-24 21:02:03 -0400

    Added more style to login, signup, and not found components

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default () => {
-  return <p>NotFound component here</p>
+  return (
+    <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h1>Page Not Found</h1>
+          <p>Hmmm, we're unable to find that page</p>
+          <button><Link to='/'>Head Home</Link></button>
+        </div>
+      </div>
+  )
 }
